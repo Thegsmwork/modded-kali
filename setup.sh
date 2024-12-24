23bdb150761ab37c2b79d96491124d66e176b718
Thegsmwork <98774023+Thegsmwork@users.noreply.github.com>
2024-12-24 14:07:59 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,209 +1,117 @@
-#!/data/data/com.termux/files/usr/bin/bash
-R="$(printf '\033[1;31m')"                           
-G="$(printf '\033[1;32m')"
-Y="$(printf '\033[1;33m')"
-B="$(printf '\033[1;34m')"
-C="$(printf '\033[1;36m')"                                        
-W="$(printf '\033[1;37m')"
+#!/data/data/com.termux/files/usr/bin/bash -e
 
+# Color codes for styling
+R="\033[1;31m"
+G="\033[1;32m"
+Y="\033[1;33m"
+B="\033[1;34m"
+C="\033[1;36m"
+W="\033[1;37m"
+
+# Display banner
 banner() {
-clear
-printf "\033[33m       █▄▀ ▄▀█ █░░ █   █▀▄▀█ █▀█ █▀▄\033[0m\n"
-printf "\033[36m       █░█ █▀█ █▄▄ █   █░▀░█ █▄█ █▄▀ \033[0m\n"
-printf "\033[32m   A modded gui of kali\033[0m\n"
-printf "\033[32m   Code by @sabamdarif \033[0m\n"
-
+    clear
+    printf "${Y}       █▄▀    ▄▀█    █░░    █    ▄▀█    █▀█    █▀▄${W}\n"
+    printf "${C}       █░█    █▀█    █▄▄    █    █▀█    █▄█    █▄▀${W}\n"
+    printf "${G}   Modded Kali NetHunter Setup${W}\n"
+    printf "${G}   By @sabamdarif${W}\n\n"
 }
 
-device_arch=$(uname -m)
-
-setup_rootfs(){
-    case "$device_arch" in
-        aarch64)
-            archtype="arm64"
-            ;;
-        arm)
-            archtype="armhf"
-            ;;
-        amd64|x86_64)
-            archtype="amd64"
-            ;;
-        i*86|x86)
-            archtype="i386"
-            ;;
+# Function to detect device architecture
+detect_architecture() {
+    printf "${B}[*] Detecting device architecture...${W}\n"
+    case "$(uname -m)" in
+        aarch64) SYS_ARCH="arm64" ;;
+        arm) SYS_ARCH="armhf" ;;
+        amd64|x86_64) SYS_ARCH="amd64" ;;
+        i*86|x86) SYS_ARCH="i386" ;;
         *)
-            echo "unknown architecture"; exit 1 ;;
+            printf "${R}Unknown architecture. Exiting.${W}\n"
+            exit 1
+            ;;
     esac
-
-base_url="https://kali.download/nethunter-images/current/rootfs/kali-nethunter-rootfs-minimal-${archtype}.tar.xz"
-rootfs_file="$(basename $base_url)"
-
-if [ -f "$rootfs_file" ]; then
-  local filename="$rootfs_file"
-  echo "${G}File found:${W} ${Y}$filename${W}"
-
-  valid_choise=false
-  while [ "$valid_choice" = false ]; do
-  echo "${G}File found:${W} ${Y}$filename${W}"
-  echo "${B}1) Remove the file${W}"
-  echo "${B}2) Rename the file${W}"
-  echo "${B}3) Continue with existing the file${W}"
-
-  read -p "${G}Enter your choice (1/2/3): ${W}" choice
-
-  case $choice in
-    1)
-      rm "$filename"
-      echo "${Y}$filename ${W}${B}File removed.${W}"
-      wget "$base_url"
-      valid_choice=true
-      ;;
-    2)
-	    new_name="kalifs-${archtype}-$(date +%s).tar.xz"
-      mv "$rootfs_file" "$new_name"
-      echo "$rootfs_file renamed to $new_name."
-	    wget "$base_url"
-        valid_choice=true
-      ;;
-    3)
-      echo "${B}Continuing with the file.${W}"
-      wget "$base_url"
-      valid_choice=true
-      ;;
-    *)
-      echo "${R}Invalid choice. Please try again.${W}"
-      ;;
-  esac
-done
-get_sha=$(sha256sum "$rootfs_file" | cut -d' ' -f1)
-else
-  echo "${G}File not found:${W} ${Y}$rootfs_file${W}"
-  wget "$base_url"
-get_sha=$(sha256sum "$rootfs_file" | cut -d' ' -f1)
-fi
-
+    printf "${G}[+] Architecture detected: ${SYS_ARCH}${W}\n"
 }
 
-add_distro(){
-banner
-    echo -e "${R} [${W}-${R}]${C} Checking required packages...${W}"
-    sed -i 's/kali/ /g' /data/data/com.termux/files/usr/bin/proot-distro
-    folder_path="/data/data/com.termux/files/usr/var/lib/proot-distro"
-
-    if [ -d "$folder_path" ]; then
-        mkdir "${folder_path}/dlcache"
-        mv "$rootfs_file" "${folder_path}/dlcache"
+# Function to set image type based on user input
+set_image_type() {
+    echo ""
+    if [[ ${SYS_ARCH} == "arm64" ]]; then
+        echo "[1] NetHunter ARM64 (full)"
+        echo "[2] NetHunter ARM64 (minimal)"
+        echo "[3] NetHunter ARM64 (nano)"
+        read -p "Enter the image you want to install: " wimg
+        case $wimg in
+            1) IMG_TYPE="full" ;;
+            2) IMG_TYPE="minimal" ;;
+            3) IMG_TYPE="nano" ;;
+            *) IMG_TYPE="full" ;;
+        esac
+    elif [[ ${SYS_ARCH} == "armhf" ]]; then
+        echo "[1] NetHunter ARMhf (full)"
+        echo "[2] NetHunter ARMhf (minimal)"
+        echo "[3] NetHunter ARMhf (nano)"
+        read -p "Enter the image you want to install: " wimg
+        case $wimg in
+            1) IMG_TYPE="full" ;;
+            2) IMG_TYPE="minimal" ;;
+            3) IMG_TYPE="nano" ;;
+            *) IMG_TYPE="full" ;;
+        esac
     else
-        mkdir "$folder_path"
-	mkdir "${folder_path}/dlcache"
-        mv "$rootfs_file" "${folder_path}/dlcache"
+        printf "${R}Unsupported architecture. Exiting.${W}\n"
+        exit 1
     fi
-
-    touch "$PREFIX/etc/proot-distro/kali.sh"
-    cat <<EOF > "$PREFIX/etc/proot-distro/kali.sh"
-# If you want to customize installation, please make a copy.
-DISTRO_NAME="Kali Linux (nethunter)"
-TARBALL_URL['$device_arch']="$base_url"
-TARBALL_SHA256['$device_arch']="$get_sha"
-EOF
-
 }
 
-check_pack() {
-	banner
-	echo -e "${R} [${W}-${R}]${C} Checking required packages..."${W}
-if [[ `command -v pulseaudio` && `command -v proot-distro` && `command -v wget` ]]; then
-        echo -e "\n${R} [${W}-${R}]${G} Packages already installed."${W}
-    else
-        packs=(pulseaudio proot proot-distro wget)
-        for pack_name in "${packs[@]}"; do
-            type -p "$pack_name" &>/dev/null || {
-                echo -e "\n${R} [${W}-${R}]${G} Installing package : ${Y}$pack_name${C}"${W}
-                apt update -y
-                apt upgrade -y
-                apt install "$pack_name" -y
-            }
+# Function to manage root filesystem setup
+setup_rootfs() {
+    local BASE_URL="https://kali.download/nethunter-images/current/rootfs"
+    local ROOTFS_FILE="kali-nethunter-rootfs-${IMG_TYPE}-${SYS_ARCH}.tar.xz"
+
+    if [ -f "$ROOTFS_FILE" ]; then
+        printf "${G}File found:${W} ${Y}$ROOTFS_FILE${W}\n"
+
+        while true; do
+            printf "${B}1) Remove the file${W}\n"
+            printf "${B}2) Rename the file${W}\n"
+            printf "${B}3) Continue with the existing file${W}\n"
+
+            read -p "${G}Enter your choice (1/2/3): ${W}" CHOICE
+            case $CHOICE in
+                1)
+                    rm "$ROOTFS_FILE"
+                    printf "${G}[+] File removed.${W}\n"
+                    break
+                    ;;
+                2)
+                    read -p "${G}Enter new name for the file: ${W}" NEW_NAME
+                    mv "$ROOTFS_FILE" "$NEW_NAME"
+                    printf "${G}[+] File renamed to $NEW_NAME.${W}\n"
+                    break
+                    ;;
+                3)
+                    printf "${G}[+] Continuing with the existing file.${W}\n"
+                    break
+                    ;;
+                *)
+                    printf "${R}Invalid choice. Please try again.${W}\n"
+                    ;;
+            esac
         done
-    fi
-
-}
-
-install_rootfs() {
-	banner
-	echo -e "${R} [${W}-${R}]${C} Setting up Environment..."${W}
-    if [[ -d "$PREFIX/var/lib/proot-distro/installed-rootfs/kali" ]]; then
-        echo -e "\n${R} [${W}-${R}]${G} Distro already installed."${W}
-        exit 0
     else
-        proot-distro install kali
-        termux-reload-settings
-    fi
-
-    if [[ -d "$PREFIX/var/lib/proot-distro/installed-rootfs/kali" ]]; then
-        echo -e "\n${R} [${W}-${R}]${G} Installed Successfully !!"${W}
-    else
-        echo -e "\n${R} [${W}-${R}]${G} Error Installing Distro !\n"${W}
-        exit 0
-    fi
-    wget https://raw.githubusercontent.com/sabamdarif/modded-kali/main/builder/gui.sh
-    if [[ -e "$PREFIX/var/lib/proot-distro/installed-rootfs/kali/root/gui.sh" ]]; then
-        rm $PREFIX/var/lib/proot-distro/installed-rootfs/kali/root/gui.sh
-        mv gui.sh $PREFIX/var/lib/proot-distro/installed-rootfs/kali/root/gui.sh
-        chmod +x $PREFIX/var/lib/proot-distro/installed-rootfs/kali/root/gui.sh
-    else
-        mv gui.sh $PREFIX/var/lib/proot-distro/installed-rootfs/kali/root/gui.sh
-        chmod +x $PREFIX/var/lib/proot-distro/installed-rootfs/kali/root/gui.sh
+        printf "${B}[*] Downloading rootfs (${IMG_TYPE} version)...${W}\n"
+        wget "$BASE_URL/$ROOTFS_FILE"
     fi
 }
 
-add_sound() {
-    banner
-    echo -e "\n${R} [${W}-${R}]${C} Fixing Sound Problem..."${W}
-    if [[ ! -e "$HOME/.kali-sound-service" ]]; then
-        touch $HOME/.kali-sound-service
-    fi
+# Main execution flow
+banner
 
-    echo "pulseaudio --start --exit-idle-time=-1" > $HOME/.kali-sound-service
-    echo "pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" >> $HOME/.kali-sound-service
-}
+detect_architecture
 
-setup_termux_x11() {
-    banner
-    echo "${R}[${W}-${R}]${G} Configuring Termux:X11 "${W}
-    pkg install x11-repo -y
-    pkg install termux-x11-nightly -y
-}
+set_image_type
 
-finish() {
-    echo "proot-distro login kali --bind /dev/null:/proc/sys/kernel/cap_last_last --shared-tmp --fix-low-ports" > $PREFIX/bin/kali
-    if [[ -e "$PREFIX/bin/kali" ]]; then
-        chmod +x $PREFIX/bin/kali
-        termux-reload-settings
-    fi
-    wget -O $HOME/remove-kali.sh https://raw.githubusercontent.com/sabamdarif/modded-kali/main/remove.sh
-    proot-distro login kali -- /bin/bash -c "mv /usr/sbin/telinit /usr/sbin/telinit.bak"
-    proot-distro login kali -- /bin/bash -c "ln -s /usr/bin/true /usr/sbin/telinit"
-}
-
-notes() {
-    if [[ -e "$PREFIX/bin/kali" ]]; then
-	echo -e "\n${R} [${W}-${R}]${G} kali-latest(CLI) is now Installed on your Termux"${W}
-        echo -e "\n${R} [${W}-${R}]${G} Type ${C}kali${G} to run kali CLI."${W}
-        echo -e "\n${R} [${W}-${R}]${G} If you Want to Use kali in GUI MODE then ,"${W}
-        echo -e "\n${R} [${W}-${R}]${G} Run ${C}kali${G} first & then type ${C}bash gui.sh "${W}
-        echo -e "\n"
-        exit 0
-    else
-        echo -e "\n${R} [${W}-${R}]${G} Error Installing Distro !"${W}
-        exit 0
-    fi
-}
-
-check_pack
 setup_rootfs
-add_distro
-install_rootfs
-setup_termux_x11
-add_sound
-finish
-notes
+
+printf "${G}[=] Modded Kali NetHunter setup complete.${W}\n"
